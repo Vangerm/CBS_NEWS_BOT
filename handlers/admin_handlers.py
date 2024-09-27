a6f3f2ff8f113e94feeaf011d18ae5a6ff32aa46
Anton <ANTON-1996@yandex.ru>
2024-09-27 17:20:15 +0300

    include admin_handlers

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -2,13 +2,11 @@ from aiogram import Router
 from aiogram.types import Message, FSInputFile
 from aiogram.filters import Command
 from filters.filters import IsAdmin
-from aiogram.fsm.context import FSMContext
 
 
 router = Router()
 
 
 @router.message(Command(commands='getlog'), IsAdmin())
-async def admin_get_log_command(message: Message, state: FSMContext):
-    await state.clear()
+async def admin_get_log_command(message: Message):
     await message.answer_document(FSInputFile('loger/logs.log'))
