7182cedad2d22206cf5a8f11892569f15ca1b634
Anton <anton-1996@ya.ru>
2024-10-10 18:42:18 +0300

    move admin handler

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,8 +1,13 @@
+import vk_api
+import asyncio
+import logging
 from aiogram import Router
 from aiogram.types import Message, FSInputFile
 from aiogram.filters import Command
 from filters.filters import IsAdmin
 
+
+logger = logging.getLogger(__name__)
 
 router = Router()
 
@@ -10,3 +15,49 @@ router = Router()
 @router.message(Command(commands='getlog'), IsAdmin())
 async def admin_get_log_command(message: Message):
     await message.answer_document(FSInputFile('loger/logs.log'))
+
+
+@router.message(Command(commands='startbot'), IsAdmin())
+async def process_start_bot_command(
+                                    message: Message,
+                                    telegram_group_id,
+                                    vk_bot_token,
+                                    vk_group_id
+                                    ):
+    logger.info('Start check vk news')
+
+    post_id = 0
+
+    # подключение по токену
+    vk_session = vk_api.VkApi(token=vk_bot_token)
+    # подключение к вк
+    vk = vk_session.get_api()
+
+    while True:
+        vk_post_info = vk.wall.get(owner_id=vk_group_id, count=2)
+        is_pinned = 0
+
+        if vk_post_info['items'][0].get('is_pinned', 0):
+            is_pinned = 1
+
+        vk_post = vk_post_info['items'][is_pinned]
+
+        if vk_post['id'] != post_id:
+            post_id = vk_post['id']
+            attachments = vk_post['attachments']
+
+            if len(attachments):
+                if attachments[0]['type'] == 'photo':
+                    url = attachments[0]['photo']['orig_photo']['url']
+                photo = URLInputFile(url)
+                await message.bot.send_photo(
+                        telegram_group_id,
+                        photo,
+                        caption=vk_post['text']
+                    )
+            else:
+                await message.bot.send_message(
+                        telegram_group_id,
+                        vk_post['text']
+                    )
+        await asyncio.sleep(1800)
--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,13 +1,7 @@
-import vk_api
-import asyncio
-import logging
-from filters.filters import IsAdmin
 from aiogram import Router
 from aiogram.types import Message, URLInputFile
 from aiogram.filters import Command, CommandStart
 
-
-logger = logging.getLogger(__name__)
 
 router = Router()
 
@@ -29,49 +23,3 @@ async def process_help_command(message: Message):
 @router.message(Command(commands='getid'))
 async def process_get_id_command(message: Message):
     await message.answer(text=str(message.chat.id))
-
-
-@router.message(Command(commands='startbot'), IsAdmin())
-async def process_start_bot_command(
-                                    message: Message,
-                                    telegram_group_id,
-                                    vk_bot_token,
-                                    vk_group_id
-                                    ):
-    logger.info('Start check vk news')
-
-    post_id = 0
-
-    # подключение по токену
-    vk_session = vk_api.VkApi(token=vk_bot_token)
-    # подключение к вк
-    vk = vk_session.get_api()
-
-    while True:
-        vk_post_info = vk.wall.get(owner_id=vk_group_id, count=2)
-        is_pinned = 0
-
-        if vk_post_info['items'][0].get('is_pinned', 0):
-            is_pinned = 1
-
-        vk_post = vk_post_info['items'][is_pinned]
-
-        if vk_post['id'] != post_id:
-            post_id = vk_post['id']
-            attachments = vk_post['attachments']
-
-            if len(attachments):
-                if attachments[0]['type'] == 'photo':
-                    url = attachments[0]['photo']['orig_photo']['url']
-                photo = URLInputFile(url)
-                await message.bot.send_photo(
-                        telegram_group_id,
-                        photo,
-                        caption=vk_post['text']
-                    )
-            else:
-                await message.bot.send_message(
-                        telegram_group_id,
-                        vk_post['text']
-                    )
-        await asyncio.sleep(1800)
