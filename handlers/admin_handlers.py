ce5a0b1fb946591329a3321116929c31d1f6078d
Anton <ANTON-1996@yandex.ru>
2024-10-16 07:37:52 +0300

    poster

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -18,7 +18,67 @@ logger = logging.getLogger(__name__)
 
 router = Router()
 
-# async def poster():
+
+async def poster(
+        message: Message,
+        attachments,
+        post_text,
+        telegram_group_id):
+    if len(attachments):
+        if len(attachments) > 1:
+            photos = list()
+            id_photo = True
+            for attachment in attachments:
+                if attachment['type'] == 'photo':
+                    url = attachment['photo']['orig_photo']['url']
+                    if id_photo:
+                        id_photo = False
+                        photos.append(InputMediaPhoto(
+                            media=URLInputFile(url),
+                            caption=post_text
+                            ))
+                    else:
+                        photos.append(InputMediaPhoto(
+                            media=URLInputFile(url)
+                            ))
+            await message.bot.send_media_group(
+                    telegram_group_id,
+                    photos
+                    )
+        elif attachments[0]['type'] == 'photo':
+            url = attachments[0]['photo']['orig_photo']['url']
+            photo = URLInputFile(url)
+            await message.bot.send_photo(
+                telegram_group_id,
+                photo,
+                caption=post_text
+            )
+        elif attachments[0]['type'] == 'video':
+            await message.bot.send_message(
+                telegram_group_id,
+                post_text
+            )
+        # elif attachments[0]['type'] == 'video':
+        #     owner_id = vk_post_info['items'][0]['attachments'][0]['video']['owner_id']
+        #     video_id = vk_post_info['items'][0]['attachments'][0]['video']['id']
+        #     access_key = vk_post_info['items'][0]['attachments'][0]['video']['access_key']
+
+        #     video = vk.video.get(videos=f'{owner_id}_{video_id}_{access_key}')
+        #     url = video['items'][0]['player']
+
+        #     input_video = URLInputFile(url)
+
+        #     await message.bot.send_video(
+        #             telegram_group_id,
+        #             input_video,
+        #             caption=vk_post['text']
+        #     )
+
+    else:
+        await message.bot.send_message(
+            telegram_group_id,
+            post_text
+        )
 
 
 @router.message(Command(commands='getlog'), IsAdmin())
@@ -35,8 +95,6 @@ async def process_start_bot_command(
                                     ):
     logger.info('Start check vk news')
 
-    post_id = 0
-
     # подключение по токену
     vk_session = vk_api.VkApi(token=vk_bot_token)
     # подключение к вк
@@ -49,59 +107,8 @@ async def process_start_bot_command(
             post_text = event.obj['text']
             attachments = event.obj['attachments']
 
-            if len(attachments):
-                if len(attachments) > 1:
-                    photos = list()
-                    id_photo = True
-                    for attachment in attachments:
-                        if attachment['type'] == 'photo':
-                            url = attachment['photo']['orig_photo']['url']
-                            if id_photo:
-                                id_photo = False
-                                photos.append(InputMediaPhoto(
-                                    media=URLInputFile(url),
-                                    caption=post_text
-                                    ))
-                            else:
-                                photos.append(InputMediaPhoto(
-                                    media=URLInputFile(url)
-                                    ))
-                    await message.bot.send_media_group(
-                            telegram_group_id,
-                            photos
-                            )
-                elif attachments[0]['type'] == 'photo':
-                    url = attachments[0]['photo']['orig_photo']['url']
-                    photo = URLInputFile(url)
-                    await message.bot.send_photo(
-                        telegram_group_id,
-                        photo,
-                        caption=post_text
-                    )
-                elif attachments[0]['type'] == 'video':
-                    await message.bot.send_message(
-                        telegram_group_id,
-                        post_text
-                    )
-
-            else:
-                await message.bot.send_message(
-                    telegram_group_id,
-                    post_text
-                )
-
-    #             elif attachments[0]['type'] == 'video':
-    #                 owner_id = vk_post_info['items'][0]['attachments'][0]['video']['owner_id']
-    #                 video_id = vk_post_info['items'][0]['attachments'][0]['video']['id']
-    #                 access_key = vk_post_info['items'][0]['attachments'][0]['video']['access_key']
-
-    #                 video = vk.video.get(videos=f'{owner_id}_{video_id}_{access_key}')
-    #                 url = video['items'][0]['player']
-
-    #                 input_video = URLInputFile(url)
-
-    #                 await message.bot.send_video(
-    #                         telegram_group_id,
-    #                         input_video,
-    #                         caption=vk_post['text']
-    #                 )
+            await poster(
+                message,
+                attachments,
+                post_text,
+                telegram_group_id)
